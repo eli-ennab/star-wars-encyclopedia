803155b4addf0851663c3828154d5e9f015aea74
Eli Ennab <elinaennab@gmail.com>
2023-06-05 15:08:44 +0200

    added navigation to app and route to /films

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import FilmsPage from './pages/FilmsPage'
 import NotFoundPage from './pages/NotFoundPage'
+import Navigation from './components/Navigation'
+import Container from 'react-bootstrap/Container'
 import './assets/scss/App.scss'
 
 function App() {
     return (
         <div id="App">
-            <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="*" element={<NotFoundPage />} />
-            </Routes>
+
+            <Navigation />
+
+            <Container className="py-3">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/films" element={<FilmsPage />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Container>
         </div>
     )
 }
