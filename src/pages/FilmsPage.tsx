f4df1782b64d4730b2768284ebe173e340baf9eb
Eli Ennab <elinaennab@gmail.com>
2023-06-05 15:07:42 +0200

    created films page

---
+++ src/pages/FilmsPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from '../components/Search'
+
+const FilmsPage = () => {
+    return (
+        <>
+            <h1>Search for all films</h1>
+
+            <Search />
+        </>
+    )
+}
+
+export default FilmsPage
