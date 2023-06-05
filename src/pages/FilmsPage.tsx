e8a0eabc79a4ebbb4c9cb5f67e42aaad7f551759
Eli Ennab <elinaennab@gmail.com>
2023-06-05 16:29:47 +0200

    implemented logic (params for example) to this project from today's lesson

--- src/pages/FilmsPage.tsx
+++ src/pages/FilmsPage.tsx
@@ -1,14 +1,99 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Alert from 'react-bootstrap/Alert'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { useSearchParams } from 'react-router-dom'
+import { search } from '../services/StarWarsAPI'
+import { SW_FilmsResponse } from '../types'
 import Search from '../components/Search'
 
 const FilmsPage = () => {
-    return (
-        <>
-            <h1>Search for all films</h1>
+	const [error, setError] = useState<string|null>(null)
+	const [loading, setLoading] = useState(false)
+	const [page, setPage] = useState(1)
+	const [searchInput, setSearchInput] = useState("")	// input field
+	const [searchResult, setSearchResult] = useState<SW_FilmsResponse|null>(null)	// data that gets back from api
+	const [searchParams, setSearchParams] = useSearchParams()	// search query in url *after* /search?
 
-            <Search />
-        </>
-    )
+	// get "search=" from URL Search Params
+	const query = searchParams.get('search')
+
+	const searchSWFilms = async (searchQuery: string, searchPage = 1) => {
+		setError(null)
+		setLoading(true)
+		setSearchResult(null)
+
+		try {
+			const res = await search(searchQuery, searchPage)
+			setSearchResult(res)
+		} catch (err: any) {
+			setError(err.message)
+		}
+
+		setLoading(false)
+	}
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault()
+
+		if (!searchInput.trim().length) {
+			return
+		}
+
+		// reset page state
+		setPage(1)
+
+		// set input value as search in searchParams
+		setSearchParams( { search: searchInput } )
+
+		// search
+		searchSWFilms(searchInput, 1)
+	}
+
+	// react to changes in page state
+	useEffect(() => {
+		if (!query) {
+			return
+		}
+
+		searchSWFilms(query, page)
+	}, [page, query])
+
+	return (
+		<>
+			<h1>Star Wars films search</h1>
+
+			<Search
+				value={searchInput}
+				onChange={e => setSearchInput(e.target.value)}
+				onSubmit={handleSubmit}
+			/>
+
+			{ error && <Alert variant="secondary">{error}</Alert>}
+
+			{ loading && <p>Loading...</p>}
+
+			{ searchResult && (
+				<div id="search-result">
+					<p>Showing {searchResult.data.length} search results for "{query}"...</p>
+
+					<ListGroup className="mb-3">
+						{searchResult.data.map(data => (
+							<ListGroup.Item
+								action
+								href={searchResult.first_page_url}
+								key={data.id}
+							>
+								<h2 className="h3">{data.title}</h2>
+								<p className="text-muted small mb-0">
+									director: {data.director} 
+								</p>
+							</ListGroup.Item>
+						))}
+					</ListGroup>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default FilmsPage
