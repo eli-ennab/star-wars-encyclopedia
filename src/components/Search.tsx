69298b6819b7ad6c54cb678d270ff4ec0a9f91c1
Eli Ennab <elinaennab@gmail.com>
2023-06-05 16:26:50 +0200

    search component is fully working

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,59 +1,38 @@
-import React from 'react'
-import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
-const Search = () => {
-    // const [error, setError] = useState<string|null>(null)
-	// const [loading, setLoading] = useState(false)
+interface IProps {
+    value: string
+    onChange: (e: any) => void
+    onSubmit: (e: React.FormEvent) => void
+}
 
-	const [searchInput, setSearchInput] = useState('')
-
-    const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault()
-
-		if (!searchInput.trim().length) {
-			return
-		}
-
-		try {
-		} catch (err) {
-		}
-	}
-
+const Search: React.FC<IProps> = ( { value, onChange, onSubmit } ) => {
     return (
         <>
-            <Form
-                    className="mb-4"
-                    onSubmit={() => {handleSubmit}}
-                >
-                    <Form.Group 
-                        className="m-4" 
-                        controlId="search"
-                        >
-                            <Form.Label>
-                                Search
-                            </Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter your search query"
-                                onChange={e => setSearchInput(e.target.value)}
-                                value={searchInput}
-                                required
-                            />
-                    </Form.Group>
+            <Form className="mb-4" onSubmit={onSubmit}>
+				<Form.Group className="mb-3" controlId="searchQuery">
+					<Form.Label>Search Film</Form.Label>
+					<Form.Control
+						onChange={onChange}
+						placeholder="Enter your search"
+						required
+						type="text"
+						value={value}
+					/>
+				</Form.Group>
 
-                    <Button
-                            variant="light"
-                            size="lg"
-                            type="submit"
-                            className="mx-4" 
-                            disabled={!searchInput.trim().length}>
-                                Search
-                        </Button>
-                </Form>
+				<div className="d-flex justify-content-end">
+					<Button
+						variant="light"
+						type="submit"
+						disabled={!value.trim().length}
+					>Search</Button>
+				</div>
+			</Form>
         </>
     )
 }
 
 export default Search
+
