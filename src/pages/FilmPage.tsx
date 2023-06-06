a451b8e2c49d2c66d5dfa1bafb0fbf75566ed2fe
Eli Ennab <elinaennab@gmail.com>
2023-06-06 14:42:56 +0200

    new type SW_Film with lists of characters, planets etcetera

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -1,7 +1,7 @@
 import { useNavigate, useLocation } from 'react-router-dom'
 import { Link, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import { SW_Films } from '../types'
+import { SW_Film } from '../types'
 import { getResource, search } from '../services/StarWarsAPI'
 import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
@@ -10,13 +10,13 @@ const FilmPage = () => {
     const location = useLocation()
     const navigate = useNavigate()
     const [error, setError] = useState<string|null>(null)
-    const [resource, setResource] = useState<SW_Films|null>(null)
+    const [resource, setResource] = useState<SW_Film|null>(null)
     const { id } = useParams()
 	const resourceId = Number(id)
 
     const getFilm = async (id: number) => {
         try {
-            const data = await getResource<SW_Films|null>('/films', id)
+            const data = await getResource<SW_Film|null>('/films', id)
             setResource(data)
             console.log(data)
         } catch (err: any) {
@@ -48,6 +48,7 @@ const FilmPage = () => {
                                     <p className=""><strong>Producer:</strong> {resource.producer}</p>
                                     <p className=""><strong>Opening crawl:</strong> {resource.opening_crawl}</p>
                                     <p className=""><strong>Release date:</strong> {resource.release_date}</p>
+                                    {/* <p className=""><strong>Characters:</strong> {resource.characters.map(characters => characters.name)}</p> */}
                                     <Button
                                         className="my-3"
                                         variant="dark"
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -30,3 +30,35 @@ export type SW_FilmsResponse = {
     to: number
     total: number
 }
+
+export type SW_Film = {
+    id: number
+    title: string
+    episode_id: number
+    opening_crawl: string
+    director: string
+    producer: string
+    release_date: string
+    created: string
+    edited: string
+    characters: [
+        id: number,
+        name: string
+    ]
+    planets: [
+        id: number,
+        name: string
+    ]
+    starships: [
+        id: number,
+        name: string
+    ]
+    vehicles: [
+        id: number,
+        name: string
+    ]
+    species: [
+        id: number,
+        name: string
+    ]
+}
