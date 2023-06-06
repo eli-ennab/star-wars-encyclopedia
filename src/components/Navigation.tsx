c985d5944f016c6701b1c2527b6b3759182af3f5
Eli Ennab <elinaennab@gmail.com>
2023-06-06 14:02:02 +0200

    better looking navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -5,7 +5,7 @@ import { NavLink, Link } from 'react-router-dom'
 
 const Navigation = () => {
 	return (
-			<Navbar bg="dark" variant="dark" expand="md">
+			<Navbar variant="dark" expand="md">
 				<Container>
 					<Navbar.Brand as={Link} to="/">Star Wars Encyclopedia</Navbar.Brand>
 
