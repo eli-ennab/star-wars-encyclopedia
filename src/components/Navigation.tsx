88270fb747a1f6466aae959b10e44d3e1a190e57
Eli Ennab <elinaennab@gmail.com>
2023-06-06 14:50:01 +0200

    added all categories to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,6 +13,11 @@ const Navigation = () => {
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
 							<Nav.Link as={NavLink} end to="/films">Films</Nav.Link>
+							<Nav.Link as={NavLink} end to="/people">People</Nav.Link>
+							<Nav.Link as={NavLink} end to="/planets">Planets</Nav.Link>
+							<Nav.Link as={NavLink} end to="/species">Species</Nav.Link>
+							<Nav.Link as={NavLink} end to="/starships">Starships</Nav.Link>
+							<Nav.Link as={NavLink} end to="/vehicles">Vehicles</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
