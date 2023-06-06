5825acec960e8298555babf35414b7af673d8f96
Eli Ennab <elinaennab@gmail.com>
2023-06-06 13:58:59 +0200

    removed margin on p tag in homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ const HomePage = () => {
     return (
         <>
             <h1>Welcome to the Star Wars Encyclopedia</h1>
-            <p className="mx-4">Search for all films, people, planets, species, starships, vehicles</p>
+            <p className="">Search for all films, people, planets, species, starships, vehicles</p>
         </>
     )
 }
