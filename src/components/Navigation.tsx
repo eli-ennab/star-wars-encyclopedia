88fd127e4843dbdf6c688654bed1af627a8be76c
Eli Ennab <elinaennab@gmail.com>
2023-06-08 16:42:15 +0200

    a better looking header!

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,7 +7,7 @@ const Navigation = () => {
 	return (
 			<Navbar variant="dark" expand="md">
 				<Container>
-					<Navbar.Brand as={Link} to="/">Star Wars Encyclopedia</Navbar.Brand>
+					<Navbar.Brand as={Link} to="/" className="navbar-header">Star Wars Encyclopedia</Navbar.Brand>
 
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
